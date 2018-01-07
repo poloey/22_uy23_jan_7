f355beb38cee168f8a9459a4f66fa39c5a33ed7c
polo dev <polodev10@gmail.com>
2018-01-07 15:16:22 +0600

    added class 22

---
+++ dynamic_navigation/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container">
+
+    <div class="row">
+        <div class="col-md-8">
+            <div class="jumbotron">
+                <h1>this is home page</h1>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sed dolore consequuntur ut ipsum odit natus, est officiis alias quaerat maiores delectus assumenda ducimus obcaecati asperiores autem et. Nulla, tempore.</p>
+        </div>
+        <div class="col-md-4">
+            <h2>Sidebar</h2>
+            <ul>
+                <li>technology</li>
+                <li>technology</li>
+                <li>technology</li>
+                <li>technology</li>
+            </ul>
+        </div>
+    </div>
+
+</div>
+<footer class="text-center bg-info text-white p-5">
+    &copy; Feni College.
+</footer>
+    
+</body>
+</html>
